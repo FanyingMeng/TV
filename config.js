aba842441aa87d48c8928f31a4edba1a7c9d154e
FanyingMeng <fanying.meng@hotmail.com>
2026-05-06 17:21:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const TV_CONFIG = {
     episode: "第20集",
     // 这里的链接一定要找视频网站“正片”的直达链接
     videoUrl: "https://v.qq.com/x/cover/x6f5q2qbrov7n39/8mSjUJDeYLC.html", 
-    webhookUrl: "你的机器人地址" // 如果不需要通知，可以留空
+    webhookUrl: "https://discord.com/api/webhooks/1501512621700939879/TIpd9vgkZlk3f914I9NcViNo32yAN45JOEuf9_l5s9j2Lu3f5Jfv00hKnjanZZ199Fer" // 如果不需要通知，可以留空
 };
