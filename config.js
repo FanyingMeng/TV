29be8a0bbdc4c54a67dd9973e5e4bc17c3f768bc
FanyingMeng <fanying.meng@hotmail.com>
2026-05-06 17:21:32 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+const TV_CONFIG = {
+    dramaName: "甄嬛传",
+    episode: "第20集",
+    // 这里的链接一定要找视频网站“正片”的直达链接
+    videoUrl: "https://v.qq.com/x/cover/x6f5q2qbrov7n39/8mSjUJDeYLC.html", 
+    webhookUrl: "你的机器人地址" // 如果不需要通知，可以留空
+};
